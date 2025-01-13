579442a518389b25a3b94a48cde1014b49f87016
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-13 16:17:05 +0530

    locals

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,4 +1,4 @@
-locals {
+locals { #locals are also used to hold any type of expressions
   ami_id = "ami-041e2ea9402c46c32"
   sg_id = "sg-0ae3a12e7b7696e53" # replace with your security group ID
   instance_type = var.instance_name == "db" ? "t2.micro" : "t2.small"
