5ab405960e3368b5d6506a131ef749045365d08f
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-13 16:07:28 +0530

    creating R53 records

--- expense/r53.tf
+++ expense/r53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "expense" {
-  count = length(var.instance_names) # three records will be created with the help of variable instance_names 
+  count = length(var.instance_names) # three records will be created with the help of length(var.instance_names) 
   zone_id = var.zone_id
   name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}" # ${}are for adding string to variables
   #name = local.record_name
@@ -10,7 +10,7 @@ resource "aws_route53_record" "expense" {
   # records will be created based on frontend. if the instance name is frontend then record will be created with public_ip
   # if the instance name is not frontend  then record will be created with private_ip
   records = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
-  # if records already exists
+  # if records already exists, then it will override the records with latest values
   allow_overwrite = true
 }
 
