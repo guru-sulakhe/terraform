430f9e645331234acfedfc8886d0a706bee18a68
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-14 14:25:02 +0530

    dynamic blocks

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "allow_ports" {
 
     #terraform block
     dynamic "ingress" { # looping ingress block
-        for_each = var.inbound_rules # looping resource inbound_rules for 4 times which is based on the list items
+        for_each = var.inbound_rules # looping resource inbound_rules for 4 times which is based on the number of list items
         content { # this block will be looped based on the list items
             from_port        = ingress.value["port"] # block-name.value["key-name"]
             to_port          = ingress.value["port"]
